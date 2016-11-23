3fc50d2d4cb863719deeca4a372aaf4f82a62b4d
Vittorio Rigamonti <rigazilla@gmail.com>
2016-11-23 10:59:17 +0100

    Example with constant hash and failures

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,36 @@
 // A basic sample application using Junction.
 
 #include <junction/ConcurrentMap_Grampa.h>
+#include <iostream>
 
-struct Foo {
+class Foo {
+  public:
+	Foo(int a) : a(a) {}
+  int a;
+};
+
+struct IntKeyTraits {
+    typedef int Key;
+    typedef typename turf::util::BestFit<int>::Unsigned Hash;
+    static const Key NullKey = Key(0);
+    static const Hash NullHash = Hash(0);
+    static Hash hash(int key) {
+         return 42;
+    }
 };
 
 void testMap() {
     // Create the map.
-    typedef junction::ConcurrentMap_Grampa<turf::u32, Foo*> ConcurrentMap;
+    typedef junction::ConcurrentMap_Grampa<turf::u32, Foo*, IntKeyTraits> ConcurrentMap;
     ConcurrentMap myMap;
 
     // Do a few operations.
-    myMap.assign(14, new Foo);
-    Foo* foo = myMap.get(14);
-    foo = myMap.exchange(14, new Foo);
-    delete foo;
-    foo = myMap.erase(14);
-    delete foo;
+    myMap.assign(14, new Foo(1));
+    myMap.assign(15, new Foo(2));
+    Foo* foo14 = myMap.get(14);
+    Foo* foo15 = myMap.get(15);
+    std::cout << "Value for key 14 is " << foo14->a << std::endl;
+    std::cout << "Value for key 15 is " << foo14->a << std::endl;
 }
 
 int main() {
